acf7e7017208d368386c554d5ad09999e2fa37d0
Joylean Anna Kingston <joyleanannakingston10@gmail.com>
2023-04-11 10:22:29 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
+import cardData from './cardsdata.json';
+import cardData2 from './cardsdatasecond.json';
+
+interface Carddata {  
+  id: Number;  
+  data: String;  
+  title: String;  
+  para: String;  
+  button: String;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'drag-drop-project';
+  carddata:Carddata[]=cardData;
+  carddatatwo:Carddata[]=cardData2;
+
+  constructor(){
+    console.log(this.carddata);
+  }
+
+  drop(event: CdkDragDrop<any>) {
+    if(event.previousContainer!==event.container){
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex)
+    }else{
+      moveItemInArray(this.carddata, event.previousIndex, event.currentIndex);
+    }
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
